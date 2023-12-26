56a34b075cf9f6bfe412a5388bf90eebcf55fcfd
j14884480 <j14884480@gmail.com>
2023-12-26 14:34:36 +0800

    修复字符串变元祖的错误

--- app/view_models/book.py
+++ app/view_models/book.py
@@ -1,19 +1,18 @@
 class BookViewModel:
     def __init__(self, book):
         self.title = book['title']
-        self.publisher = str(book['publisher']),
-        self.pages = book['pages'] or '',
-        self.image = book['image'],
-        self.price = book['price'],
-        self.summary = str(book['summary']),
-        self.author = '。'.join(book['author'])
+        self.publisher = str(book['publisher'])
+        self.pages = book['pages'] or ''
+        self.image = book['image']
+        self.price = book['price']
+        self.summary = book['summary'] or ''
+        self.author = '、'.join(book['author'])
 
     @property
     def intro(self):
         intros = filter(lambda x: True if x else False, [self.author, self.publisher, self.price])
 
-        # print('/'.join(intros))
-        return 'a'
+        return '/'.join(intros)
 
 
 class BookCollection:
@@ -26,40 +25,3 @@ class BookCollection:
         self.total = yushu_book.total
         self.keyword = keyword
         self.books = [BookViewModel(book) for book in yushu_book.books]
-
-# class _BookViewModel:
-#     @classmethod
-#     def package_single(cls, data, keyword):
-#         returned = {
-#             'books': [],
-#             'total': 0,
-#             'keyword': keyword
-#         }
-#         if data:
-#             returned['total'] = 1
-#             returned['books'] = [cls._cut_book_data(data)]
-#         return returned
-#
-#     @classmethod
-#     def package_collection(cls, data, keyword):
-#         returned = {
-#             'books': [],
-#             'total': 0,
-#             'keyword': keyword
-#         }
-#         if data:
-#             returned['total'] = data['total']
-#             returned['books'] = [cls._cut_book_data(book) for book in data['books']]
-#         return returned
-#
-#     @classmethod
-#     def _cut_book_data(cls, data):
-#         book = {
-#             'title': data['title'],
-#             'publisher': data['publisher'],
-#             'pages': data['pages'] or '',
-#             'image': data['image'],
-#             'summary': data['summary'] or '',
-#             'author': '、'.join(data['author'])
-#         }
-#         return book
